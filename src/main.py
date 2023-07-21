5eb6ef6c25e902d4f1df0eee2d84f4624b77732d
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-07-21 03:08:35 +0200

    Fixed Threshold funtion and added frame_difference() function

--- src/main.py
+++ src/main.py
@@ -10,13 +10,20 @@ def get_frame(capture):
         return None
 
 
-def otsu_thresholding(frame):
+def threshold_frame(frame):
     """Convert the frame to grayscale, apply Gaussian filter,
-       and then use Otsu's thresholding algorithm."""
+       and then use threshold algorithm."""
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gaussian_filter = cv2.GaussianBlur(gray_frame, (5, 5), 0)
-    _, threshold = cv2.threshold(gaussian_filter, 0, 255,
-                                 cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, threshold = cv2.threshold(gaussian_filter, 25, 255,
+                                 cv2.THRESH_BINARY)
+    return threshold
+
+
+def frame_difference(src1, src2):
+    """TODO"""
+    diff_frame = cv2.absdiff(src1=src1, src2=src2)
+    threshold = threshold_frame(diff_frame)
     return threshold
 
 
@@ -34,12 +41,19 @@ def camera_start(port):
     if capture.isOpened() is False:
         print('Error opening the camera')
 
+    prev_frame = None
+
     while capture.isOpened():
         frame = get_frame(capture)
+
         if frame is not None:
-            cv2.imshow('Raw camera view', frame)
-            threshold = otsu_thresholding(frame)
-            cv2.imshow('Otsu + Gaussian filter', threshold)
+            if prev_frame is not None:
+                threshold = frame_difference(frame, prev_frame)
+
+                cv2.imshow('Raw camera view', frame)
+                cv2.imshow('Threshold + Gaussian filter', threshold)
+
+            prev_frame = frame
 
             if cv2.waitKey(20) & 0xFF == ord('q'):
                 break
