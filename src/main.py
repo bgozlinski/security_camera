e3d3323435881a3f28e0d62e77732194832726ba
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-07-24 02:19:51 +0200

    Added background subtraction using MOG2 and improved documentation.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,16 @@ import numpy
 
 
 def get_frame(capture):
-    """Capture and return the real frame from the camera."""
+    """
+    Capture and return the real frame from the camera.
+
+    Args:
+    - capture: The VideoCapture object.
+
+    Returns:
+    - frame: Captured frame.
+    """
+
     ret, frame = capture.read()
     if ret:
         return frame
@@ -12,6 +21,17 @@ def get_frame(capture):
 
 
 def camera_start(port):
+    """
+    Initialize and start the camera.
+
+    Args:
+    - port: The camera port (usually 0 for default camera).
+
+    Returns:
+    - capture: The VideoCapture object.
+    - fps: Frames per second of the capture.
+    """
+
     capture = cv2.VideoCapture(port)
 
     frame_width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -29,46 +49,53 @@ def camera_start(port):
 
 
 def camera_stop(capture):
+    """
+    Release the camera and close any OpenCV windows.
+
+    Args:
+    - capture: The VideoCapture object.
+    """
+
     capture.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
+    # Start the camera
     camera, fps = camera_start(port=0)
 
-    prev_frame = None
+    # Initialize the Background Subtractor once
+    fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 
-    for _ in range(5):  # Discard first 5 frames, for instance
-        get_frame(camera)
-
-    fgbg = cv2.createBackgroundSubtractorMOG2()
-    # fgbg = cv2.createBackgroundSubtractorKNN()
-
+    # Main loop to process each frame
     while camera.isOpened():
         frame = get_frame(camera)
 
-        if frame is not None:
-            if prev_frame is not None:
-                fgmask = fgbg.apply(frame)
-                # Find contours in the thresholded difference image
-                contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                # Iterate over the contours and draw bounding rectangles
-                for contour in contours:
-                    if cv2.contourArea(contour) < 2500:  # This threshold is arbitrary, you can adjust based on your needs
-                        continue
-                    (x, y, w, h) = cv2.boundingRect(contour)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Apply the background subtractor to get the foreground mask
+        fgmask = fgbg.apply(frame)
 
-                cv2.imshow('Raw camera view', frame)
-                cv2.imshow('fgmask', fgmask)
+        # Find contours in the fgmask to detect moving objects
+        contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:
+            # Get the largest contour
+            max_contour = max(contours, key=cv2.contourArea)
 
-            prev_frame = frame
+            # If the contour area is above a threshold, draw a bounding rectangle around it
+            if cv2.contourArea(max_contour) > 5000:
+                (x, y, w, h) = cv2.boundingRect(max_contour)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+            # Display the original frame and the foreground mask
+            cv2.imshow('Raw camera view', frame)
+            cv2.imshow('fgmask', fgmask)
+
+            # Break the loop if 'q' is pressed
             if cv2.waitKey(20) & 0xFF == ord('q'):
                 break
         else:
             break
 
+    # Stop the camera and release resources
     camera_stop(camera)
 
 
