bf443f028f3627faaf07d67d6fd313ebbaf51c05
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-07-25 00:09:43 +0200

    Feature: Add red dot to indicate motion center
    Implemented a red dot visualization at the center of detected motion in the video feed. This helps in quickly identifying the core region of movement in the frame.

--- src/main.py
+++ src/main.py
@@ -59,6 +59,35 @@ def camera_stop(capture):
     cv2.destroyAllWindows()
 
 
+def highlight_motion_center(frame, fgmask):
+    """
+    Draw a dot on the center of the detected movement.
+
+    Args:
+    - frame: The current frame from the video capture.
+    - fgmask: The foreground mask after background subtraction.
+    """
+
+    # Find contours in the fgmask to detect moving objects
+    contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        # Get the largest contour
+        max_contour = max(contours, key=cv2.contourArea)
+
+        # If the contour area is above a threshold, draw a red dot
+        if cv2.contourArea(max_contour) > 5000:
+            (x, y, w, h) = cv2.boundingRect(max_contour)
+            center_x = int(x + (w / 2))
+            center_y = int(y + (h / 2))
+
+            # Draw a red dot (circle) at the center of the moving object
+            cv2.circle(img=frame,
+                       center=(center_x, center_y),
+                       radius=5,
+                       color=(0, 0, 255),
+                       thickness=-1)
+
+
 if __name__ == "__main__":
     # Start the camera
     camera, fps = camera_start(port=0)
@@ -68,33 +97,22 @@ if __name__ == "__main__":
 
     # Main loop to process each frame
     while camera.isOpened():
+        # Get the current frame
         frame = get_frame(camera)
 
         # Apply the background subtractor to get the foreground mask
         fgmask = fgbg.apply(frame)
 
-        # Find contours in the fgmask to detect moving objects
-        contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        if contours:
-            # Get the largest contour
-            max_contour = max(contours, key=cv2.contourArea)
+        # Draw dot on detected movement
+        highlight_motion_center(frame, fgmask)
 
-            # If the contour area is above a threshold, draw a bounding rectangle around it
-            if cv2.contourArea(max_contour) > 5000:
-                (x, y, w, h) = cv2.boundingRect(max_contour)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Display the combined frame (original frame + foreground mask)
+        combined_frame = np.hstack((frame, cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)))
+        cv2.imshow('View', combined_frame)
 
-            # Display the original frame and the foreground mask
-            combined_frame = np.hstack((frame, cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)))
-            cv2.imshow('View', combined_frame)
-
-            # Break the loop if 'q' is pressed
-            if cv2.waitKey(20) & 0xFF == ord('q'):
-                break
-        else:
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(20) & 0xFF == ord('q'):
             break
 
-    # Stop the camera and release resources
+    # Release camera and resources
     camera_stop(camera)
-
-
