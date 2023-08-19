eafa8c4dd6d9f33ba677c4aad6d3b034d355bce8
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-08-19 13:06:24 +0200

    Added camera.isOpened() method on start of main loop and changed while camera.isOpened() to while True.

--- src/main.py
+++ src/main.py
@@ -6,27 +6,35 @@ from datetime import datetime, timedelta
 
 
 if __name__ == "__main__":
-    # Start the camera
+    # Start the camera.
     camera, fps = camera_start(port=0)
 
-    # Initialize the Background Subtractor once
+    # Quick check if camera is available before proceeding.
+    if not camera.isOpened():
+        print("Camera initialization failed!")
+        exit()  # <-- Error handling added
+
+    # Initialize the Background Subtractor once.
     fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 
     # Set the initial save time to the current time.
     next_save_time = datetime.now()
 
-    # Counter to skip frames for optimization
+    # Counter to skip frames for optimization.
     frame_skip = 0
 
-    # Main loop to process each frame
-    while camera.isOpened():
-        # Get the current frame
+    # User feedback prompt.
+    print('Press "q" to stop the video feed')
+
+    # Main loop to process each frame.
+    while True:
+        # Get the current frame.
         frame = capture_frame(camera)
 
-        # Resize frame for better performance
-        frame = resize_frame(frame)  # <-- Frame Resizing
+        # Resize frame for better performance.
+        frame = resize_frame(frame)
 
-        # Apply the background subtractor to get the foreground mask
+        # Apply the background subtractor to get the foreground mask.
         fgmask = fgbg.apply(frame)
 
         # If there's notable motion (validated by `highlight_motion_center`), check the save conditions.
@@ -39,14 +47,14 @@ if __name__ == "__main__":
                 # capture_video(capture=camera, duration=10)
                 next_save_time = current_time + timedelta(seconds=10)
 
-        # Display the combined frame (original frame + foreground mask)
+        # Display the combined frame (original frame + foreground mask).
         combined_frame = np.hstack((frame, cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)))
         cv2.imshow(winname='frame', mat=combined_frame)
 
-        # Skip some frames for performance optimization
+        # Skip some frames for performance optimization.
         frame_skip += 1
         if frame_skip % 3 == 0:
-            # Break the loop if 'q' is pressed
+            # Break the loop if 'q' is pressed.
             if cv2.waitKey(20) & 0xFF == ord('q'):
                 break
 
