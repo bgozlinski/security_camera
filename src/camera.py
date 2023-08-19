b05b57f953eab379b29569d8696b59955295fa89
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-08-19 12:57:45 +0200

    Added resize_frame function to optimize frame processing

--- src/camera.py
+++ src/camera.py
@@ -151,3 +151,9 @@ def capture_video(capture, duration=None):
         cv2.destroyAllWindows()
 
     return True
+
+
+def resize_frame(frame, scale_percent=50):
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    return cv2.resize(frame, (width, height))  # <-- New Function
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from camera import camera_start, camera_stop, capture_frame, capture_image, capture_video
+from camera import camera_start, camera_stop, capture_frame, capture_image, capture_video, resize_frame
 from motion_detect import highlight_motion_center
 from datetime import datetime, timedelta
 
@@ -20,6 +20,9 @@ if __name__ == "__main__":
         # Get the current frame
         frame = capture_frame(camera)
 
+        # Resize frame for better performance
+        frame = resize_frame(frame)  # <-- Frame Resizing
+
         # Apply the background subtractor to get the foreground mask
         fgmask = fgbg.apply(frame)
 
@@ -29,12 +32,13 @@ if __name__ == "__main__":
 
             # If the current time is beyond the next save time, save the frame and reset the timer.
             if current_time >= next_save_time:
-                capture_image(frame=frame)
-                capture_video(capture=camera, duration=10)
+                # capture_image(frame=frame)
+                # capture_video(capture=camera, duration=10)
                 next_save_time = current_time + timedelta(seconds=10)
 
-        # # Display the combined frame (original frame + foreground mask)
-        # combined_frame = np.hstack((frame, cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)))
+        # Display the combined frame (original frame + foreground mask)
+        combined_frame = np.hstack((frame, cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)))
+        cv2.imshow(winname='frame', mat=combined_frame)
 
         # Break the loop if 'q' is pressed
         if cv2.waitKey(20) & 0xFF == ord('q'):
