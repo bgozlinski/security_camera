3da5d9a28e725f8e041e43f7cc056191a78a0d04
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-09-02 20:52:11 +0200

    Refactor MotionDetect class to separate motion detection and dot drawing

--- src/motion_detect.py
+++ src/motion_detect.py
@@ -7,44 +7,33 @@ class MotionDetect:
     """
     A class to handle motion detection.
     """
-    def highlight_motion_center(self, frame: np.ndarray, fgmask: np.ndarray, dot_radius: int = 5, dot_color: Tuple[int, int, int] = (0, 0, 255), area_threshold: int = 5000) -> float:
-        """
-        Highlights the center of motion in a given frame.
 
-        Args:
-            frame (numpy.ndarray): The frame to analyze.
-            fgmask (numpy.ndarray): The foreground mask.
-            dot_radius (int): The radius of the dot to draw.
-            dot_color (tuple): The color of the dot.
-            area_threshold (int): The area threshold for motion detection.
-
-        Returns:
-            int: The area of the detected motion.
-        """
+    def find_motion(self, fg_mask: np.ndarray, area_threshold: int = 5000) -> Tuple[float, Tuple[int, int]]:
         # Noise Reduction.
         kernel = np.ones((5, 5), np.uint8)
-        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+        fgmask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, kernel)
 
         # Find contours in the fgmask.
-        contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(fg_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         if contours:
-            # Get the largest contour.
+            # Get the largest contour
             max_contour = max(contours, key=cv2.contourArea)
 
-            # If the contour area is above the threshold, draw the dot.
+            # If the contour area is above the threshold
             if cv2.contourArea(max_contour) > area_threshold:
                 (x, y, w, h) = cv2.boundingRect(max_contour)
                 center_x = int(x + (w / 2))
                 center_y = int(y + (h / 2))
 
-                # Draw the dot at the center of the moving object.
-                cv2.circle(img=frame,
-                           center=(center_x, center_y),
-                           radius=dot_radius,
-                           color=dot_color,
-                           thickness=-1)
+                return cv2.contourArea(max_contour), (center_x, center_y)
 
-                return cv2.contourArea(max_contour)
+        return None
 
-        return 0
+    def draw_dot_on_motion(self, frame: np.ndarray, center_coordinates: Tuple[int, int], dot_radius: int = 5, dot_color: Tuple[int, int, int] = (0, 0, 255)):
+        # Draw the dot at the center of the moving object
+        cv2.circle(img=frame,
+                   center=center_coordinates,
+                   radius=dot_radius,
+                   color=dot_color,
+                   thickness=-1)
