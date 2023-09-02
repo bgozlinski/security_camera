5b1b1bcc8c3bed34cc3d0a5f35bfb9e826baea36
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-09-02 13:30:13 +0200

    Moved configurations variables to config_loader.py

--- src/config/config_loader.py
+++ src/config/config_loader.py
@@ -4,4 +4,27 @@ import configparser
 def load_config(file_path):
     config = configparser.ConfigParser()
     config.read(file_path)
-    return config
+
+    camera_port = int(config.get('DEFAULT', 'CameraPort', fallback='0'))
+    frame_resize_percent = int(config.get('DEFAULT', 'FrameResizePercent', fallback='50'))
+    save_interval_seconds = int(config.get('DEFAULT', 'SaveIntervalSeconds', fallback='10'))
+    frame_skip_count = int(config.get('DEFAULT', 'FrameSkip', fallback='1'))
+    capture_image_enabled = config['DEFAULT'].getboolean('CaptureImage')
+    capture_video_enabled = config['DEFAULT'].getboolean('CaptureVideo')
+
+    motion_area_threshold = int(config.get('MOTION', 'MotionAreaThreshold', fallback='500'))
+    dot_radius = int(config.get('MOTION', 'DotRadius', fallback='5'))
+    dot_colour = tuple(map(int, config.get('MOTION', 'DotColour', fallback="(0, 0, 255)").strip('()').split(',')))
+
+    return {
+        'camera_port': camera_port,
+        'frame_resize_percent': frame_resize_percent,
+        'save_interval_seconds': save_interval_seconds,
+        'frame_skip_count': frame_skip_count,
+        'capture_image_enabled': capture_image_enabled,
+        'capture_video_enabled': capture_video_enabled,
+        'motion_area_threshold': motion_area_threshold,
+        'dot_radius': dot_radius,
+        'dot_colour': dot_colour
+    }
+
--- src/main.py
+++ src/main.py
@@ -10,22 +10,11 @@ def main():
     """
     Main script to handle security camera operations.
     """
+    # Fetching configurations.
     config = load_config('config/config.ini')
 
-    # Fetching configurations
-    camera_port = int(config.get('DEFAULT', 'CameraPort', fallback='0'))
-    frame_resize_percent = int(config.get('DEFAULT', 'FrameResizePercent', fallback='50'))
-    save_interval_seconds = int(config.get('DEFAULT', 'SaveIntervalSeconds', fallback='10'))
-    frame_skip_count = int(config.get('DEFAULT', 'FrameSkip', fallback='1'))
-    capture_image_enabled = config['DEFAULT'].getboolean('CaptureImage')
-    capture_video_enabled = config['DEFAULT'].getboolean('CaptureVideo')
-
-    motion_area_threshold = int(config.get('MOTION', 'MotionAreaThreshold', fallback='500'))
-    dot_radius = int(config.get('MOTION', 'DotRadius', fallback='5'))
-    dot_colour = tuple(map(int, config.get('MOTION', 'DotColour', fallback="(0, 0, 255)").strip('()').split(',')))
-
     # Start the camera.
-    camera = Camera(port=camera_port)
+    camera = Camera(port=config['camera_port'])
 
     if not camera.is_opened:
         print("Failed to open camera. Exiting.")
@@ -38,7 +27,7 @@ def main():
     motion_detector = MotionDetect()
 
     # Set the initial save time to future in save_interval_seconds second.
-    next_save_time = datetime.now() + timedelta(seconds=save_interval_seconds)
+    next_save_time = datetime.now() + timedelta(seconds=config['save_interval_seconds'])
 
     # Counter to skip frames for optimization.
     frame_skip = 0
@@ -49,7 +38,7 @@ def main():
         frame = camera.capture_frame()
 
         # Resize frame for better performance.
-        frame = camera.resize_frame(frame, scale_percent=frame_resize_percent)
+        frame = camera.resize_frame(frame, scale_percent=config['frame_resize_percent'])
 
         # Apply the background subtractor to get the foreground mask.
         fgmask = fgbg.apply(frame)
@@ -57,22 +46,22 @@ def main():
         # Get contour area.
         motion_area = motion_detector.highlight_motion_center(frame=frame,
                                                               fgmask=fgmask,
-                                                              dot_radius=dot_radius,
-                                                              dot_color=dot_colour,
-                                                              area_threshold=motion_area_threshold)
+                                                              dot_radius=config['dot_radius'],
+                                                              dot_color=config['dot_colour'],
+                                                              area_threshold=config['motion_area_threshold'])
 
         # Check for motion based on a defined threshold
         if motion_area:
             current_time = datetime.now()
 
             if current_time >= next_save_time:
-                if capture_image_enabled:
+                if config['capture_image_enabled']:
                     camera.capture_image(frame=frame)
 
-                if capture_video_enabled:
-                    camera.capture_video(duration=save_interval_seconds)
+                if config['capture_video_enabled']:
+                    camera.capture_video(duration=config['save_interval_seconds'])
 
-                next_save_time = current_time + timedelta(seconds=save_interval_seconds)
+                next_save_time = current_time + timedelta(seconds=config['save_interval_seconds'])
 
         # Display the combined frame (original frame + foreground mask).
         combined_frame = np.hstack((frame, cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)))
@@ -80,13 +69,14 @@ def main():
 
         # Skip some frames for performance optimization.
         frame_skip += 1
-        if frame_skip % frame_skip_count == 0:
+        if frame_skip % config['frame_skip_count'] == 0:
             # Break the loop if 'q' is pressed.
             if cv2.waitKey(20) & 0xFF == ord('q'):
                 break
 
     # Release camera and resources
     camera.stop_camera()
+
+
 if __name__ == "__main__":
     main()
-
